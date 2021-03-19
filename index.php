8fa388fdbedf455ed56adbf340e80c4c940dd61a
Jjurgita <jurgitabalinaite@gmail.com>
2021-03-19 12:52:11 +0200

    Added primary style

--- index.php
+++ index.php
@@ -5,27 +5,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
     <title>Project Manager</title>
 </head>
 
 <body>
 
-    <header>
-        <div>
+    <header class="header">
+        <div class="title">
             <h1>Sprint #2 - Project Manager</h1>
         </div>
-        <div>
+        <div class="nav">
             <a href="?path=projects">Projects</a>
         </div>
-        <div>
+        <div class="nav">
             <a href="?path=employees">Employees</a>
         </div>
     </header>
     <?php
     if (isset($_GET['path'])) {
         if ($_GET['path'] == 'projects') {
+            print('<div class="tbl_name"><h2 class="tbl_name">Projects Table</h2></div>');
             include 'projects.php';
         } else if ($_GET['path'] == 'employees') {
+            print('<div class="tbl_name"><h2 class="tbl_name">Employees Table</h2></div>');
             include 'employees.php';
         }
     }
